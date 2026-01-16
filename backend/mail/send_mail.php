44287293945ed2679d708483df46753d6db8867c
jeni-y <jenidhason2005@gmail.com>
2026-01-16 15:19:42 +0530

    nwe

---
+++ backend/mail/send_mail.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__ . '/Mailer.php';
+
+if ($argc < 4) {
+    exit("Usage: php send_mail.php recipient subject body\n");
+}
+
+$to = $argv[1];
+$subject = $argv[2];
+$body = $argv[3];
+
+Mailer::send($to, $subject, $body);
