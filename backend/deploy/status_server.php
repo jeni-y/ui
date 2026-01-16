f80c7abd58a670daaf5e7638de84e3e7c7423d3d
Ajay <ajay0032004j@gmail.com>
2026-01-16 16:12:06 +0530

    Remove db.php requirement from status_server.php
    Removed database configuration requirement from status_server.php.

--- backend/deploy/status_server.php
+++ backend/deploy/status_server.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__ . '/../bootstrap.php';
-require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../middleware/auth_check.php';
 require __DIR__ . '/../../../vendor/autoload.php';
 
